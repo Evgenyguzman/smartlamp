9ab3d3cfcaafbff55133d1c09c753e2ff01ac2b7
Evgenii <eguzman@yandex.ru>
2020-05-06 15:29:55 +0300

    Update Led.py

--- Led.py
+++ Led.py
@@ -26,9 +26,10 @@ class Led(object):
 		G_val = (col & 0x001100) >> 8
 		B_val = (col & 0x000011) >> 0
 		print(R_val, G_val, B_val)
-		R_val = map(R_val, 0, 255, 0, 100)
-		G_val = map(G_val, 0, 255, 0, 100)
-		B_val = map(B_val, 0, 255, 0, 100)
+		# R_val = map(R_val, 0, 255, 0, 100)
+		# G_val = map(G_val, 0, 255, 0, 100)
+		# B_val = map(B_val, 0, 255, 0, 100)
+		# print(R_val, G_val, B_val)
 		self.p_R.ChangeDutyCycle(100-R_val)     # Change duty cycle
 		self.p_G.ChangeDutyCycle(100-G_val)
 		self.p_B.ChangeDutyCycle(100-B_val)
